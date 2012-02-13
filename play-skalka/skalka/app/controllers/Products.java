ebda0a1ed155cd3cedb439a382c3e88079d89dea
Valentin Kravtsov <balivali@yahoo.com>
2012-02-13 12:17:18 +0000

    Parsing

--- play-skalka/skalka/app/controllers/Products.java
+++ play-skalka/skalka/app/controllers/Products.java
@@ -1,19 +1,27 @@
 package controllers;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
+
+import aux.Constants;
+import aux.html_parser.ParsedProductData;
+import aux.html_parser.ProductParser;
 
 import play.mvc.Controller;
 
 public class Products extends Controller {
-
+	
 	public static void parseUrl(String url) {
-		Map<String, String> testResponse = new HashMap<String, String>();
+		try {
+			renderJSON(new ProductParser().parse(url));
+		} catch (Exception e) {
+			e.printStackTrace();
+			renderJSON(Constants.ERROR_KEY + new Integer(Constants.ERROR_PARSING_FAILED));
+		}
 
-		testResponse.put("url", url);
-		testResponse.put("myKey", "myValue");
-
-		renderJSON(testResponse);
+		
 
 	}
 
