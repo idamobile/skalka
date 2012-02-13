0795cdc19df39c4b39f3e832ec8f28a859b933c8
zjor <zjor.se@gmail.com>
2012-02-13 14:33:10 +0100

    x

--- play-skalka/skalka/app/controllers/Application.java
+++ play-skalka/skalka/app/controllers/Application.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import models.User;
+import play.cache.Cache;
 import play.mvc.Before;
 import play.mvc.Controller;
 
@@ -7,10 +9,10 @@ public class Application extends Controller {
 
 	@Before
 	static void checkConnected() {
-		if (!session.contains("user")) {
+		if (!session.contains(User.JSON_TAG_ACCESS_TOKEN)) {
 			Signin.index();
 		} else {
-			renderArgs.put("user", session.get("user"));
+			renderArgs.put("user", Cache.get(session.get(User.JSON_TAG_ACCESS_TOKEN)));
 		}
 	}
 
@@ -19,7 +21,7 @@ public class Application extends Controller {
 	}
 
 	public static void logout() {
-		session.remove("user");
+		session.remove(User.JSON_TAG_ACCESS_TOKEN);
 		index();
 	}
 
