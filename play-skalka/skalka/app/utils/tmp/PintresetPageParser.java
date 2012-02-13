953782ce4bb00ac1b699b2d982d3daed1c8a783d
Valentin Kravtsov <balivali@yahoo.com>
2012-02-13 19:28:10 +0000

    small fix

--- play-skalka/skalka/app/utils/tmp/PintresetPageParser.java
+++ play-skalka/skalka/app/utils/tmp/PintresetPageParser.java
@@ -19,7 +19,7 @@ import utils.Pair;
 
 public class PintresetPageParser {
 	
-	private static final String FILE ="http://pinterest.com/ilnem/categories/";
+	private static final String FILE ="http://pinterest.com/ilnem/categories?page=3";
 	
 	public static final void main(String[] args) throws IOException, ClassNotFoundException, SQLException{
 		Document doc = Jsoup.connect(FILE).get();
@@ -39,21 +39,31 @@ public class PintresetPageParser {
 		// Setup the connection with the DB
 		Connection connect = DriverManager.getConnection("jdbc:mysql://localhost/skalka?" + "user=skalka&password=skalka");		
 		for(String key : result.keySet()){
-			PreparedStatement preparedStatement = connect.prepareStatement("insert into categories values (default, ?, 'http://www.google.com')", Statement.RETURN_GENERATED_KEYS);
-			preparedStatement.setString(1, key);
-			long categoryId = 0;
-			try{
-				preparedStatement.execute();
-				ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
-		        if (generatedKeys.next()) {
-		            categoryId = generatedKeys.getLong(1);
-		        } else {
-		            throw new SQLException("Creating user failed, no generated key obtained.");
-		        }
-			} catch(Exception e) {
-				e.printStackTrace();
-				System.out.println("could not add category:" + key);
+			Statement statement = connect.createStatement();
+			ResultSet catResult = statement.executeQuery("select id from categories where name='"+key+"'");
+			PreparedStatement preparedStatement;
+			long categoryId;
+			if(!catResult.next()){
+				preparedStatement = connect.prepareStatement("insert into categories values (default, ?, 'http://www.google.com')", Statement.RETURN_GENERATED_KEYS);
+				preparedStatement.setString(1, key);
+				categoryId = 0;
+				try{
+					preparedStatement.execute();
+					ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+			        if (generatedKeys.next()) {
+			            categoryId = generatedKeys.getLong(1);
+			        } else {
+			            throw new SQLException("Creating user failed, no generated key obtained.");
+			        }
+				} catch(Exception e) {
+					e.printStackTrace();
+					System.out.println("could not add category:" + key);
+				}
+			
+			} else {
+				categoryId = catResult.getLong(1);
 			}
+			statement.close();
 			preparedStatement = connect.prepareStatement("insert into subcategories values (default, ?, ?, ?)");
 			for(Pair<String,String> subcat : result.get(key)){
 				preparedStatement.setString(1, subcat.fst);
