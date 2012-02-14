99ab1ce263233c6c3cf68f1f29aa27f83002b9c8
Valentin Kravtsov <balivali@yahoo.com>
2012-02-14 21:42:29 +0000

    intermediate result

--- play-skalka/skalka/app/models/ProductInList.java
+++ play-skalka/skalka/app/models/ProductInList.java
@@ -1,15 +1,24 @@
 package models;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinColumns;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import org.hibernate.annotations.ForeignKey;
 
 import play.db.jpa.Model;
 
 @Entity
-@Table(name = "list_prod")
+@Table(name = "list_prod", uniqueConstraints = { @UniqueConstraint(columnNames = { 
+        "list_id", "product_id" }) })
 public class ProductInList extends Model {
 
 	@Column(name = "list_id")
@@ -18,6 +27,12 @@ public class ProductInList extends Model {
 	@Column(name = "product_id")
 	public Long productId;
 
+	@OneToMany
+	@JoinColumns( {
+		@JoinColumn(name="list_id"), 
+		@JoinColumn(name="product_id")
+	})
+	public List<UserActionsInProductList> userActions;
 
 	public ProductInList() {
 	}
