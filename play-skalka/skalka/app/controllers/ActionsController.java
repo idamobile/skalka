c2cb3a32aa938841c8a7b3f86234e6612c24b796
zjor <zjor.se@gmail.com>
2012-02-15 12:05:20 +0100

    x

--- play-skalka/skalka/app/controllers/ActionsController.java
+++ play-skalka/skalka/app/controllers/ActionsController.java
@@ -1,13 +1,30 @@
 package controllers;
 
-import models.UserCategories;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import models.Category;
+import models.Subcategory;
 import play.mvc.Controller;
 
 public class ActionsController extends Controller {
 
 	public static void all() {
 
-		renderText(UserCategories.count("byUserId", 2L));
+		List<Subcategory> cats = Subcategory.all().fetch();
+
+		Map<Category, List<Subcategory>> c = new HashMap<Category, List<Subcategory>>();
+
+		for (Subcategory sub : cats) {
+			if (!c.containsKey(sub.category)) {
+				c.put(sub.category, new ArrayList<Subcategory>());
+			}
+			c.get(sub.category).add(sub);
+		}
+
+		renderJSON(c);
 
 	}
 
