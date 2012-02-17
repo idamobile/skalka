10f05d1a9b9648822fe7864d56284ac8a4e264a4
Valentin Kravtsov <balivali@yahoo.com>
2012-02-17 18:27:48 +0000

    x

--- play-skalka/skalka/app/controllers/Lists.java
+++ play-skalka/skalka/app/controllers/Lists.java
@@ -138,7 +138,7 @@ public class Lists extends Application {
 	}
 
 	public static void addUserAction(Long listId, Long productId, String userAction) {
-		JPAQuery query = ProductInList.find("listId = ? AND productId = ? ", listId, productId);
+		JPAQuery query = ProductInList.find("list_id = ? AND product_id = ? ", listId, productId);
 		ProductInList pil = query.first();
 		if (pil == null) {
 			renderJSON(new ErrorResult(-1, "product not in list"));
