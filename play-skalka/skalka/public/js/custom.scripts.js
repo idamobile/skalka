b79762862d71b08a7298b56905afdf728b375959
HCB DEV <hcb@project.idamob.ru>
2012-02-17 13:32:50 +0300

    x

--- play-skalka/skalka/public/js/custom.scripts.js
+++ play-skalka/skalka/public/js/custom.scripts.js
@@ -80,30 +80,6 @@ $(document).ready(function ($) {
 		}
 	});
 
-	// Initializing INVITE FRIENDS popup
-	$('.inviteFriends').fancybox({
-		fitToView: false,
-		scrolling: 'no',
-		padding: 0,
-		//openEffect : 'elastic',
-		openSpeed  : 150,
-
- 		beforeLoad: function() { 
-			alert("invite friends");
-		},
-		//closeEffect : 'elastic',
-		closeSpeed  : 150,
-		minHeight: 0,
-		wrapCSS: 'skalkaModal',
-		helpers : {
-			overlay : {
-				css : {
-					'background-color' : '#eee'
-				},
-				opacity: 0.5
-			}
-		}
-	});
 
 	$('.productInFeed').click(function (event) {
 		event.preventDefault();
